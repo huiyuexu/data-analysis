5a63619fa9c9c7fdfe517c23ee70963d9f5b8770
amos-hsu <751654494@qq.com>
2018-01-16 05:37:42 -0800

    pandas

---
+++ tools/pandas_test.py
@@ -0,0 +1,10 @@
+#-*- coding: utf-8 -*-
+
+import pandas as pd
+
+s = pd.Series([1,2,3], index=['a', 'b', 'c'])
+d = pd.DataFrame([[1,2,3], [3,4,5]],columns = ['a','b','c'])
+ds = pd.DataFrame(s)
+
+d.head()
+d.describe()
