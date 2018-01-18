2ad55f549c18e0d68f2d5d36926136e415758cca
amos-hsu <751654494@qq.com>
2018-01-18 16:12:58 +0800

    interplate

--- data_preprocess/lagrange_newton_interplate.py
+++ data_preprocess/lagrange_newton_interplate.py
@@ -12,19 +12,23 @@ inputfile = './data/catering_sale.xls' #销量数据路径
 outputfile = './tmp/sales.xls' #输出数据路径
 
 data = pd.read_excel(inputfile)
+#data[u'日期'].to_excel('./tmp/sales0.xls')
 #异常值过滤，变为空值
-data['销量'][data['销量']<400 | data['销量']>5000] = None
+#null_raw = list((data['销量']<400) | (data['销量']>5000))
+#data.loc[:, '销量'][(data['销量']<400) | (data['销量']>5000)] = None
+data.loc[(data['销量']<400) | (data['销量']>5000), '销量'] = None
+#data.to_excel('./tmp/sales1.xls')
 
 #自定义列向量插值函数
 def polyinterp_column(s, n, k=5):
-    y = s 
-    
-    
+    y = s [list(range(n-k, n)) + list(range(n+1, n+1+k))]
+    y = y[y.notnull()]    #剔除空值
+    return lagrange(y.index, list(y))(n)    #插值并返回结果
 
-    
 for i in data.columns:
     for j in range(len(data)):
-        if data[i].isnull():
-            data[i][j] = polyinterp_column(data[i], j)
+        if (data[i].isnull())[j]:
+            #data[i][j] = polyinterp_column(data[i], j)
+            data.loc[j, [i]] = polyinterp_column(data[i], j)
 
 data.to_excel(outputfile)
