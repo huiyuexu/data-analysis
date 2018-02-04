9af22bcdc56822df0c4d559378037af7f8fbcff7
amos-hsu <751654494@qq.com>
2018-02-04 17:02:39 +0800

    KMeans

--- data_modeling/kmeans.py
+++ data_modeling/kmeans.py
@@ -33,6 +33,7 @@ r_detail = pd.concat(
 r_detail.columns = list(data.columns) + [u'聚类类别']
 #print(r_detail)
 
+'''
 #自定义作图函数
 def density_plot(data):
     import matplotlib.pyplot as plt
@@ -51,3 +52,25 @@ fig_output = './tmp/kmeans_pd_'
 for i in range(k):
     data_r = data[r_detail[u'聚类类别'] == i].iloc[:, 1:]
     density_plot(data_r).savefig(u'%s%s'%(fig_output, i))
+'''
+
+#对kmeans结果可视化展示
+from sklearn.manifold import TSNE
+
+tsne = TSNE()
+#数据降维度
+tsne.fit_transform(data_zs)
+tsne = pd.DataFrame(tsne.embedding_, index = data_zs.index)
+
+import matplotlib.pyplot as plt
+plt.rcParams['font.sans-serif'] = ['SimHei'] #用来正常显示中文标签
+plt.rcParams['axes.unicode_minus'] = False #用来正常显示负号
+
+#不同类别用不同颜色和样式绘图
+d = tsne[r_detail[u'聚类类别'] == 0]
+plt.plot(d[0], d[1], 'r.')
+d = tsne[r_detail[u'聚类类别'] == 1]
+plt.plot(d[0], d[1], 'go')
+d = tsne[r_detail[u'聚类类别'] == 2]
+plt.plot(d[0], d[1], 'b*')
+plt.show()
